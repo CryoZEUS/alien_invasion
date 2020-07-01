4c8a2f32dc358f5d8e81961de1722c9431145760
CryoZEUS <yashbhardwaj911@gmail.com>
2020-07-01 23:14:39 +0530

    dropped fleet, changing direction

--- settings.py
+++ settings.py
@@ -21,6 +21,7 @@ class Settings:
 
         #Alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 1
+        #fleet_drop_speed speed at which fleet drops screen when an alien reaches either edge
+        self.fleet_drop_speed = 10
         #fleet_direction of 1 implies right, -1 implies left.
         self.fleet_direction = 1
